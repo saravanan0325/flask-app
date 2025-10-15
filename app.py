30d9979a947b6b6606ab2f405ad711918d07737f
Saravanan_0325 <ce.24503716@gmail.com>
2025-10-15 12:30:19 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, Render!"
+    return "Hello, 🥰Saravanan 😊Render!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
